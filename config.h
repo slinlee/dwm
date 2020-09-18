a6e8fcee9299c6f8b875f32999faaf20fa9b972f
Slin Lee <slinlee@gmail.com>
2020-09-18 12:07:29 -0700

    Adjusting gap width again

--- config.h
+++ config.h
@@ -7,15 +7,15 @@
 /* appearance */
 static const unsigned int borderpx  = 30;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
-static const unsigned int gappih    = 30;       /* horiz inner gap between windows */
+static const unsigned int gappih    = 100;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 30;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 80;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 70;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappoh    = 150;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 140;       /* vert outer gap between windows and screen edge */
 static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=20:antialias=true:autohint=true"  };
 static char dmenufont[]             = "monospace:size=10";
 static char normbgcolor[]           = "#6C5B7B";
 static char normbordercolor[]       = "#355C7D";
@@ -33,8 +33,8 @@ typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
-const char *spcmd1[] = {TERMINAL, "-n", "spterm", "-g", "120x34", NULL };
-const char *spcmd2[] = {TERMINAL, "-n", "spcalc", "-f", "monospace:size=16", "-g", "50x20", "-e", "bc", "-lq", NULL };
+const char *spcmd1[] = {TERMINAL, "-n", "spterm", "-f", "monospace:size=36", "-g", "120x34", NULL };
+const char *spcmd2[] = {TERMINAL, "-n", "spcalc", "-f", "monospace:size=36", "-g", "50x20", "-e", "bc", "-lq", NULL };
 static Sp scratchpads[] = {
 	/* name          cmd  */
 	{"spterm",      spcmd1},
